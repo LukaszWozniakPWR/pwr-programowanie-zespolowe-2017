8a56e32947c5aa7933d697b116d464245b863a70
Jakub Banasiak <jakub.banasiak@infullmobile.com>
2017-12-28 13:34:26 +0100

    next

---
+++ android/app/src/main/java/com/pwr/zespolowe2016/cardgame/menu/animation/MenuAnimationAttributes.kt
@@ -0,0 +1,7 @@
+package com.pwr.zespolowe2016.cardgame.menu.animation
+
+class MenuAnimationAttributes(var springInterpolatorFactor: Float,
+                              var durationOfFirstAppLogoAnimation: Long,
+                              var startDelayOfFirstAppLogoAnimation: Long,
+                              var durationOfCommonViewsAnimation: Long,
+                              var startDelayOfCommonViewsAnimation: Long)
