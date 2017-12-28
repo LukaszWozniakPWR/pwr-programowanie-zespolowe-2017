5c1eaf553bab0205ed43ecbc08e9a4030cb8f9a7
Jakub Ściga <jkbscg@gmail.com>
2017-12-28 16:26:02 +0100

    test

--- model/src/main/java/cardgame/model/Cards.java
+++ model/src/main/java/cardgame/model/Cards.java
@@ -1,9 +1,25 @@
 package cardgame.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public enum Cards {
-    ARCHER(new Card(new ArrayList<>(), 5, null, null));
+    ARCHER(new Card(new ArrayList<>(), 5, null, null)),
+
+    TEST1(new Card(new ArrayList<Attribute>(Arrays.asList(Attribute.SPY)), 2,
+            null, null)),
+
+    TEST2(new Card(new ArrayList<>(), 3, MusterClass.NEKKER, null)),
+
+    TEST3(new Card(new ArrayList<Attribute>(Arrays.asList(Attribute.BOND)), 4,
+            null, BondClass.NAUSICAA)),
+
+    TEST4(new Card(new ArrayList<Attribute>(Arrays.asList(Attribute.MUSTER)), 5,
+            MusterClass.ELVEN_SKIRMISHER, null)),
+
+    TEST5(new Card(new ArrayList<Attribute>(Arrays.asList(Attribute.SCORCH)), 5,
+            null, BondClass.NAUSICAA));
 
     private Card card;
     Cards(Card card) {
